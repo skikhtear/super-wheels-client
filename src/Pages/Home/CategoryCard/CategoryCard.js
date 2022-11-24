6687cbf9ecb5ca2a2c27a209250dedf1fa9a122e
skikhtear <skikhtear@gmail.com>
2022-11-25 01:24:54 +0600

    card

--- src/Pages/Home/CategoryCard/CategoryCard.js
+++ src/Pages/Home/CategoryCard/CategoryCard.js
@@ -3,10 +3,10 @@ import React from 'react';
 const CategoryCard = ({ category }) => {
     const { name, picture } = category;
     return (
-        <div className="card bg-base-100 shadow-xl image-full h-52 w-72">
-            <figure><img src={picture} alt="Shoes" /></figure>
+        <div className="card card-compact w-auto bg-base-100 shadow-xl ">
+            <figure><img src={picture} alt="Shoes"  style={{height:280,width:500}}/></figure>
             <div className="card-body">
-                <h2 className="card-title">{name}</h2>
+                <h2 className="card-title text-3xl font-bold">{name}</h2>
             </div>
         </div>
     );
