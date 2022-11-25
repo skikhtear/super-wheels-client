ac8cf011c02ef5a6b8ba2a101500f0cb941f7741
skikhtear <skikhtear@gmail.com>
2022-11-26 01:32:15 +0600

    post card info

--- src/Pages/SellPostCard/SellPostCard.js
+++ src/Pages/SellPostCard/SellPostCard.js
@@ -5,13 +5,20 @@ const SellPostCard = ({ sellPost }) => {
     return (
             <div className="card card-compact w-auto bg-base-100 shadow-xl mx-5 ">
                 <figure><img src={image} alt="Shoes" style={{ height: 280, width: 500 }} /></figure>
-                <div className="card-body">
-                <h2 className="card-title text-3xl font-bold">{productName}</h2>
-                <p>Original price: ${originalPrice}</p>
-                <p>Resale price: ${resalePrice}</p>
-                <p>Years of use: {yearsOfUse}</p>
-                <p>{originalPrice}</p>
-                <p>Original price: ${originalPrice}</p>
+            <div className="card-body d-flex">
+                <div className='d-flex'>
+                    <div>
+                        <h2 className="card-title text-3xl font-bold">{productName}</h2>
+                        <p>Original price: ${originalPrice}</p>
+                        <p>Resale price: ${resalePrice}</p>
+                        <p>Years of use: {yearsOfUse}</p>
+                        <p>Location: {location}</p>
+                        <p>Seller name:{sellerName}</p>
+                    </div>
+                    <div>
+                        <button className='btn btn-primary'>Book Now</button>
+                    </div> 
+                  </div>
                 </div>
             </div>
     );
